5f4988cf8affa39aad0baef92826362b849dac7a
Sohaib Snouber <sohaibshehabsnouber@gmail.com>
2024-02-17 17:46:17 +0100

    Initial commit

---
+++ build/turtlebot/CMakeFiles/FollowLine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu/Desktop/catkin_ws/devel/lib/turtlebot/FollowLine"
+  "/home/ubuntu/Desktop/catkin_ws/devel/lib/turtlebot/FollowLine.pdb"
+  "CMakeFiles/FollowLine.dir/src/FollowLine.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FollowLine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
